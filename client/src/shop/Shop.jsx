ae131c5f819a2d6627cd217630965b20bd9bd379
teerapanjai <140907636+teerapanjai@users.noreply.github.com>
2023-12-18 22:26:28 +0700

    Update Shop.jsx

--- client/src/shop/Shop.jsx
+++ client/src/shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
   const [books, setBooks] = useState([]);
@@ -15,23 +16,23 @@ const Shop = () => {
       <h2 className="text-5xl font-bold text-center">All book are here</h2>
 
       <div className="grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1">
-        {books.map((books) =>
+        {books.map((books) => (
           <Card>
             <img src={books.imageURL} alt="" className="w-96" />
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              <p>
-                {books.bookTitle}
-              </p>
+              <p>{books.bookTitle}</p>
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              <p>
-                {books.bookDescription}
-              </p>
+              <p>{books.bookDescription}</p>
             </p>
-
-            <button className="bg-blue-700 font-semibold text-white py-2 rounded">Buy Now</button>
+            <Link
+              className="bg-blue-700 font-semibold text-white py-2 rounded text-center"
+              to={`/book/${books.bookID}`}
+            >
+              <button>Buy Now</button>
+            </Link>
           </Card>
-        )}
+        ))}
       </div>
     </div>
   );
